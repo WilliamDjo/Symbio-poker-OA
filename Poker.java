7b663b71786d145d58440defaf83a9ea12a0cdf0
WilliamDjo <william.djong@yahoo.com>
2025-06-01 20:08:14 +1000

    Add Card class with value and suit properties

--- Poker.java
+++ Poker.java
@@ -1,6 +1,14 @@
 import java.util.*;
 
 public class Poker {
+    static class Card {
+        int value;
+        char suit;
+
+        Card(String card) {
+
+        }
+    }
 
     public static void main(String[] args) {
     
