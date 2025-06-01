386b5f3a43268e465e30480ee99bb15805fa0182
WilliamDjo <william.djong@yahoo.com>
2025-06-01 20:08:49 +1000

    Add card value mapping for 2-9, T, J, Q, K, A

--- Poker.java
+++ Poker.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
 public class Poker {
+    // hash map to store card values
+    private static final HashMap<Character, Integer> CARD_VALUE_MAP = new HashMap<>();
+    static {
+        CARD_VALUE_MAP.put('2', 2);
+        CARD_VALUE_MAP.put('3', 3);
+        CARD_VALUE_MAP.put('4', 4);
+        CARD_VALUE_MAP.put('5', 5);
+        CARD_VALUE_MAP.put('6', 6);
+        CARD_VALUE_MAP.put('7', 7);
+        CARD_VALUE_MAP.put('8', 8);
+        CARD_VALUE_MAP.put('9', 9);
+        CARD_VALUE_MAP.put('T', 10);
+        CARD_VALUE_MAP.put('J', 11);
+        CARD_VALUE_MAP.put('Q', 12);
+        CARD_VALUE_MAP.put('K', 13);
+        CARD_VALUE_MAP.put('A', 14);
+    }
     static class Card {
         int value;
         char suit;
